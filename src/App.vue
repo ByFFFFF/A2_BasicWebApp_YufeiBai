ab34ff952385a7b570237f4b21cc9265aeb68cb9
YuFei Bai <ybai0043@student.monash.edu>
2024-10-12 17:52:20 +1100

    BR.E4

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,19 @@
 <script setup>
 import BHeader from './components/BHeader.vue'
-// import Form from './components/Form.vue';
+import { ref } from 'vue'
+import html2pdf from 'html2pdf.js'
 
+const documentRef = ref(null)
+
+const exportToPDF = () => {
+  const element = documentRef.value
+  if (element) {
+    html2pdf(element, {
+      margin: 1,
+      filename: 'generated-pdf.pdf',
+    })
+  }
+}
 </script>
 
 <template>
@@ -10,13 +22,13 @@ import BHeader from './components/BHeader.vue'
   </header>
 
   <main>
-    <!-- <Form>
-    </Form> -->
-    <router-view></router-view>
+    <div ref="documentRef">
+      <router-view></router-view>
+    </div>
+
+    <button @click="exportToPDF">Export to PDF</button>
   </main>
 </template>
-
-
 
 <style scoped>
 header {
@@ -30,5 +42,18 @@ header {
 
 .about {
   text-align: center;
+}
+
+button {
+  padding: 10px 20px;
+  background-color: #4CAF50;
+  color: white;
+  border: none;
+  cursor: pointer;
+  margin-top: 20px;
+}
+
+button:hover {
+  background-color: #45a049;
 }
 </style>
